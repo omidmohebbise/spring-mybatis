348200c1aec7f947e5641642e18a021d5b42b929
omid mohebbi <omidmohebbise@gmail.com>
2024-01-06 08:46:41 +0100

    Initial the project from old repo.

---
+++ sp2.6-mybatis/src/main/java/com/omidmohebbise/todoapp/identity/model/Role.java
@@ -0,0 +1,14 @@
+package com.omidmohebbise.todoapp.identity.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+    private long id;
+    private String title;
+}
